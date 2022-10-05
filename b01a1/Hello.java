afb2b0fd099ae4e7c627c41fab7a123554a53b31
Mats Bo <mats-bo@paysen.org>
2022-10-05 23:39:13 +0200

    First and second Java Programm in EidP

---
+++ b01a1/Hello.java
@@ -0,0 +1,15 @@
+
+
+
+/*
+ * @author mpayse2s
+ * Programm: displays "Hello World!"
+ */
+
+public class Hello {
+	public static void main(String[] args) {
+	// display 
+	System.out.println("Hello World!");
+	}
+}
+
---
+++ b01a2/Ciao.java
@@ -0,0 +1,11 @@
+/*
+ * @author mpayse2s
+ *  Programm: displays "Ciao mondo!"
+ */
+
+public class Ciao {
+	public static void main(String[] args) {
+	// display
+	System.out.println("Ciao mondo!");
+	}
+}
